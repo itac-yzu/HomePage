3e4b6c49c7da4e23b4b7ab1cc5cee8da20b8dabe
Erickson <a820224@gmail.com>
2015-01-29 21:49:23 +0800

    hide logo if scroll

--- js/main.js
+++ js/main.js
@@ -5,7 +5,8 @@ $(function() {
 
 
 $(document).ready(function(e) {
-    if(typeof window.orientation === 'undefined') {
+    headerHide();
+    if (typeof window.orientation === 'undefined') {
         $('.timeline-panel > .timeline-body').hide()
         $('.timeline-panel').hover(function() {
             $('.timeline-panel:hover > .timeline-heading').css("text-align", "left");
@@ -14,6 +15,42 @@ $(document).ready(function(e) {
             $('.timeline-panel:hover > .timeline-body').toggle("slow")
         });
     } else {
-    	$('.timeline-title').css("text-align", "left");
+        $('.timeline-title').css("text-align", "left");
+
     }
 });
+
+var headerHide = function() {
+    var docElem = document.documentElement;
+    var header = $('.navbar-header');
+    var menu = $('#navigation');
+    var didScroll = false;
+    var hideHeaderOn = 70;
+
+    function init() {
+        window.addEventListener('scroll', function(event) {
+            if (!didScroll) {
+                didScroll = true;
+                setTimeout(scrollPage, 100);
+            }
+        }, false);
+    }
+
+    function scrollPage() {
+        var sy = scrollY();
+        if (sy >= hideHeaderOn) {
+            header.addClass('logo-hide');
+            menu.addClass('navigation-short');
+        } else {
+            header.removeClass('logo-hide');
+            menu.removeClass('navigation-short');
+        }
+        didScroll = false;
+    }
+
+    function scrollY() {
+        return window.pageYOffset || docElem.scrollTop;
+    }
+    if($(window).width() >= 768)
+        init();
+}
