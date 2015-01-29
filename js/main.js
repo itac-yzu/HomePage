3ed97ee9c38b2e78e1f127f8958dd366371d5131
Erickson <a820224@gmail.com>
2015-01-29 20:46:57 +0800

    fix responsive

--- js/main.js
+++ js/main.js
@@ -1,13 +1,19 @@
 $(function() {
-    $('.timeline-panel > .timeline-body').hide()
+
     $.scrollIt();
 });
 
+
 $(document).ready(function(e) {
-    $('.timeline-panel').hover(function() {
-        $('.timeline-panel:hover > .timeline-heading').css("text-align", "left");
-        $('.timeline-panel:hover > .timeline-body').toggle("slow");
-    }, function() {
-        $('.timeline-panel:hover > .timeline-body').toggle("slow")
-    });
+    if(typeof window.orientation === 'undefined') {
+        $('.timeline-panel > .timeline-body').hide()
+        $('.timeline-panel').hover(function() {
+            $('.timeline-panel:hover > .timeline-heading').css("text-align", "left");
+            $('.timeline-panel:hover > .timeline-body').toggle("slow");
+        }, function() {
+            $('.timeline-panel:hover > .timeline-body').toggle("slow")
+        });
+    } else {
+    	$('.timeline-title').css("text-align", "left");
+    }
 });
